bac753174e6489e1c1fa9baeaeb131b0d547372f
gitthuong <thuong@gmail.com>
2020-06-30 11:52:40 -0700

    add new react files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.comcast.com/-/media/Images/Hub/Articles/comcast_official_logo_2012.jpg?rev=a1408cb1-e169-4af1-9951-fdfae5a62edc&la=en&hash=DC7C0CFAA9E4082C642D3CEAA40DF67CC92061EF"  alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, my name is Thuong Nguyen!!!
         </p>
         <a
           className="App-link"
