0ebe88336583fb4ed98273568e672e7fd68279da
Thuong Nguyen <thuong@gmail.com>
2020-07-02 16:19:27 -0700

    Updates

---
+++ precache-manifest.a40c4867ecf2fb286764cd8b75cb71fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab81edcc03d9fe7b09662ad027562c55",
+    "url": "/hello-intro-react/index.html"
+  },
+  {
+    "revision": "c302137cb5663dedb4b9",
+    "url": "/hello-intro-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c54e9378a5f5c56ab2cf",
+    "url": "/hello-intro-react/static/js/2.9d58873f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-intro-react/static/js/2.9d58873f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c302137cb5663dedb4b9",
+    "url": "/hello-intro-react/static/js/main.892b6504.chunk.js"
+  },
+  {
+    "revision": "ca18ca8a9126b0a7e82b",
+    "url": "/hello-intro-react/static/js/runtime-main.09137284.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-intro-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
